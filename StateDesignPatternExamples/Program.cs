5a898ff6f72ae6c646a0a79261df3d55050673dd
gajananchavhan <gpchavan050@gmail.com>
2021-09-01 22:21:54 +0530

    InitialCommit State Design Pattern

---
+++ StateDesignPatternExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StateDesignPatternExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
